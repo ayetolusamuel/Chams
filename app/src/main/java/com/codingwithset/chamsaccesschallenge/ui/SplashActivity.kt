f2dc2763b9721c805a4ebb733862ec3921c3b325
ayetolusamuel <ayetolusamuel@gmail.com>
2020-05-15 11:01:27 +0100

    interrupt thread on user list ic clicked

--- app/src/main/java/com/codingwithset/chamsaccesschallenge/ui/SplashActivity.kt
+++ app/src/main/java/com/codingwithset/chamsaccesschallenge/ui/SplashActivity.kt
@@ -2,15 +2,9 @@ package com.codingwithset.chamsaccesschallenge.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.os.Handler
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.codingwithset.chamsaccesschallenge.R
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.TimeUnit
-import kotlin.math.log
 
 
 class SplashActivity : AppCompatActivity() {
